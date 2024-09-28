8a63168d6c5e344f1cc6db0849d939242c95acc2
Marcin Wojtczak <marcinwojtczak.pure@gmail.com>
2024-09-28 09:14:16 +0200

    feat: add hero image to assets

---
+++ src/pages/home/HeroSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HeroSection() {
+  return (
+    <div className="bg-[url('/public/hero-section.jpg')] w-full bg-contain bg-no-repeat bg-center h-[550px]">
+
+    </div>
+  )
+}
