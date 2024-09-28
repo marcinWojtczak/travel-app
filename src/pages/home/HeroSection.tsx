a603e4d79198b57d4d96c5b91a18385a7af64521
Marcin Wojtczak <marcinwojtczak.pure@gmail.com>
2024-09-28 21:12:47 +0200

    style: change bg-containe to bg-cover and change div hight

--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function HeroSection() {
   return (
-    <div className="bg-[url('/public/hero-section.jpg')] w-full bg-contain bg-no-repeat bg-center h-[550px]">
+    <div className="bg-[url('/public/hero-section.jpg')] w-full bg-cover bg-no-repeat bg-center h-[750px] rounded-2xl">
 
     </div>
   )
