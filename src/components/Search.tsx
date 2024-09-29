6e4e3f0cee457bdf07489ceb0f04b1ef02f8c995
Marcin Wojtczak <marcinwojtczak.pure@gmail.com>
2024-09-29 22:30:25 +0200

    feat: added dynamic focus handling and border

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,24 +1,33 @@
 import { Search as SearchIcon } from 'lucide-react';
-import { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, useState } from 'react';
 
 type SearchType = {
     placeholder: string,
     type: string,
-    className: string,
+    customWidth: string,
+    border: boolean
 } & InputHTMLAttributes<HTMLInputElement> 
 
-export default function Search({ placeholder, type, className, ...props }: SearchType) {
+export default function Search({ placeholder, type, customWidth, border, ...props}: SearchType) {
+  const [isFocused, setIsFocused] = useState<boolean>(false)
+
   return (
-    <div className={`${className} relative flex items-center lg:w-[360px]`}>
-    <label htmlFor='search' className='sr-only'>Search</label>
-    <input
-        id='search'
-        className='p-[10px] border rounded-2xl pl-10 outline-none w-full' 
-        placeholder={placeholder}
-        type={type}
-        {...props}
-    />
-    <SearchIcon className='absolute pl-4 w-9 pointer-events-none bottom-[9px]'/> 
+    <div className={`relative flex items-center b h-12 ${customWidth}`}>
+      <label htmlFor='search' className='sr-only'>Search</label>
+      <input
+          id='search'
+          className={`p-[10px] h-full border ${border ? 'border-dark-green' : 'border-white' } rounded-full pl-14 outline-none w-full`} 
+          placeholder={placeholder}
+          style={{ backgroundColor: isFocused ? 'rgba(255, 255, 255, 1.0)' : 'rgba(255, 255, 255, 0.4)'}}
+          type={type}
+          onFocus={() => setIsFocused(true)} 
+          onBlur={() => setIsFocused(false)}
+          {...props}
+      />
+      <div className='absolute bg-brown h-9 w-9 rounded-full flex items-center justify-center pointer-events-none ml-2'>
+        <SearchIcon className='h-4 w-4 text-white'/> 
+      </div>
+      
     </div>
   )
 }
