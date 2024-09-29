38fe78d03af279939393bd73e4383b97648b0d35
Marcin Wojtczak <marcinwojtczak.pure@gmail.com>
2024-09-29 22:26:50 +0200

    feat: added dynamic border prop

--- src/components/DesktopMenu.tsx
+++ src/components/DesktopMenu.tsx
@@ -1,12 +1,14 @@
+type DesktopMenuProps = {
+  border: boolean
+}
 
-
-export default function DesktopMenu() {
+export default function DesktopMenu({ border }: DesktopMenuProps) {
   return (
-    <nav className='hidden lg:inline'>
-        <ul className='flex gap-8'>
-        <li>Home</li>
-        <li>Discover</li>
-        <li>More</li>
+    <nav className={`hidden lg:inline border ${border ? 'border-dark-green' : 'border-white'} bg-white rounded-full px-8 py-3`} style={{ backgroundColor: 'rgba(255, 255, 255, 0.4)' }}>
+        <ul className='flex gap-8 '>
+        <li className='font-medium'>Home</li>
+        <li className='font-medium'>Discover</li>
+        <li className='font-medium'>More</li>
         </ul>
     </nav>
   )
