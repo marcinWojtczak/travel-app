20e63564e9db06a822bb03212f6c74b479d2986d
Marcin Wojtczak <marcinwojtczak.pure@gmail.com>
2024-09-29 22:34:54 +0200

    style: updated colors palette, added aBeeZee font and parallax background

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,28 +7,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        'deepBlue': '#003D5B',
-        'steelBlue': '#30638E',
-        'teal': '#00798C',
-        'red': '#D1495B',
-        'yellow': '#EDAE49',
-        'lightBlue': '#7198b8',
-        'darkBlue': '#385c7e',
-        'darkGreen': '#182c26',
-        'bottleGreen': '#495d30',
-        'green': '#4a4b0c',
-        'brown': '#ab5f54',
-        'darkBrown': '#383024',
-        'medlar': '#cfbea3',
-        'orange': '#c8560d',
-        'lightOrange': '#c8560d',
-        'gray': "#acb8c0"
+        'dark-green': '#172601',
+        'green': '#5D9122',
+        'light-green': '#B3BC0B',
+        'yellow': '#F2E205',
+        'light-yellow': '#F2EEAC',
+        'dark-yellow': '#A67D03',
+        'brown': '#d15712'
       },
       fontFamily: {
         playfair: ['"Playfair Display"', 'serif'],
         lato: ['Lato', 'sans-serif'],
+        aBeeZee: ['ABeeZee', 'sans-serif']
       },
-      
+      backgroundImage: {
+        'parallax': 'url("public/hero-section.jpg")'
+      }
     },
   },
   plugins: [],
