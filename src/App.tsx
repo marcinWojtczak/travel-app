a892cc427d323b3735c280c18f3b4df724ba1212
Marcin Wojtczak <marcinwojtczak.pure@gmail.com>
2024-09-29 22:24:49 +0200

    reafctor: removed Header component and adjusted layout in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import Header from '@components/Header'
 import HeroSection from './pages/home/HeroSection'
 
 function App() {
   
 
   return (
-    <div>
-      <Header />
-      <div className='pt-[75px] flex flex-col justify-center px-4 mx-auto'>
-        <HeroSection />
-      </div>
-      
+    <div className='h-[2500px] mx-4'>
+       <HeroSection />
     </div>
   )
 }
