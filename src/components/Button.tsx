9e896491a1fb3eae542a9702386a000b9432e763
Marcin Wojtczak <marcinwojtczak.pure@gmail.com>
2024-09-30 07:56:02 +0200

    style:added border to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ type ButtonProps = {
 export default function Button({ text, icon: Icon, buttonColor }: ButtonProps) {
   return (
     <button 
-      className='w-fit flex items-center justify-between rounded-full bg-dark-green p-2 gap-8 hover:scale-105 transition-all ease-in-out'
+      className='w-fit flex items-center justify-between rounded-full bg-dark-green p-2 gap-8 hover:scale-105 transition-all ease-in-out border border-white'
       style={{ backgroundColor: buttonColor}}  
     >
         <h6 className='pl-4 font-bold'>
