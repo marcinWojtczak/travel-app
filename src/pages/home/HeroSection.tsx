414befe240d0a5e4c0b184f8e8bbd047fa2d7485
Marcin Wojtczak <marcinwojtczak.pure@gmail.com>
2024-09-30 07:59:04 +0200

    style: changed border rounded to 3xl

--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -7,7 +7,7 @@ import Header from "@components/Header"
 export default function HeroSection() {
   return (
     <>
-    <div className='flex bg-fixed bg-parallax flex-col justify-center h-[650px] lg:h-screen gap-1 px-6 md:px-7 xl:px-10 2xl:px-[116px] bg-cover bg-center rounded-b-2xl'>
+    <div className='flex bg-fixed bg-parallax flex-col justify-center h-[650px] lg:h-screen gap-1 px-6 md:px-7 xl:px-10 2xl:px-[116px] bg-cover bg-center rounded-b-3xl'>
       <Header />
       <div className='mt-[82px] flex flex-col gap-4 lg:w-[75%]'>
         <h1 className='text-white font-black text-[40px] sm:text-[45px] md:text-[60px] lg:text-[75px] xl:text-[85px] leading-tight'>Find Your Next Destination and Let the Journey Begin</h1>
