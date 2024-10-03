07117bf4e3e18c48b687f341923449dc3ea8c2df
Marcin Wojtczak <marcinwojtczak.pure@gmail.com>
2024-10-03 20:49:32 +0200

    feat: integrate react-query and redux into the application

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {QueryClient,QueryClientProvider,} from '@tanstack/react-query'
+import store from '@store/index.ts'
+import { Provider } from 'react-redux'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+      <App />
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>,
 )
