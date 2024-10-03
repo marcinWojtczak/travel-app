094a1534d387244767281230f2964f8fc4d9bcc4
Marcin Wojtczak <marcinwojtczak.pure@gmail.com>
2024-10-03 20:47:48 +0200

    chore: add module declarations for various paths and libraries

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,1 +1,7 @@
 declare module '@components/*';
+declare module '@utils/*';
+declare module '@pages/*';
+declare module 'react-rating-stars-component';
+declare module '@reduxjs/toolkit';
+declare module '@store/*';
+declare module '@features/*';
