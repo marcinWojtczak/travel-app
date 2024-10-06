6942e9f4e6570d53d69d8dd32772d4951ff4edf8
Marcin Wojtczak <marcinwojtczak.pure@gmail.com>
2024-10-06 18:00:21 +0200

    refactor: update NearbyAttractions to use local JSON data and normalize structure

--- src/pages/home/NearbyAttractions.tsx
+++ src/pages/home/NearbyAttractions.tsx
@@ -1,17 +1,45 @@
 import Slider from '@components/Slider';
 import { useEffect, useState } from 'react';
 import { useGetAttractionsQuery } from '@utils/api/fetch-travelAdvisor-data';
-
+import SectionWithSlider from "@pages/home/SectionWithSlider";
+import attractionsDataJson from '../../data/attractionData.json';
 
 type Coordinates = {
   sw: { lat: number, lng: number},
   ne: { lat: number, lng: number},
 }
 
+type AttractionsData = {
+  location_id: string;
+  name: string;
+  latitude: string;
+  longitude: string;
+  num_reviews: string;
+  rating: string;
+  photo: {
+    images: {
+      large: {
+        url: string;
+      };
+    };
+  };
+  subcategory?: {
+    name: string;
+  }[];
+};
+
+type NormalizedItem = {
+  imageUrl: string;
+  title: string;
+  rating?: string;
+  num_reviews?: string;
+  subcategory?: string;
+};
+
 export default function NearbyAtractions() {
   const [coordinates, setCoordinates] = useState<Coordinates | null>(null)
+  const [attractionsData, setAttractionsData] = useState<AttractionsData[]>([])
     
-
     useEffect(() => {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
@@ -22,24 +50,54 @@ export default function NearbyAtractions() {
     }
     }, [])
 
-    const { isPending, isError, data } = useGetAttractionsQuery(coordinates)
-  
+    // const { isPending, isError, data } = useGetAttractionsQuery(coordinates)
     
-    if (isPending) {
-      return <h3>Is loading...</h3>
-    }
 
-    if (isError) {
-      return <h3>Error occurred while fetching data.</h3>;
-    }
+    useEffect(() => {
+      // Symulacja fetchowania danych z lokalnych plików JSON
+      const fetchAttractions = async () => {
+        try {
+          setAttractionsData(attractionsDataJson);
+        } catch (error) {
+          console.error("Error fetching attractions:", error);
+        }
+      };
 
-    if (data) {
-      return (
-        <div className='w-full max-w-[1250px] flex flex-col mx-auto x-4 lg:p-6 2xl:p-8 bg-green/10 rounded-xl'>
-            <h3 className='font-semibold'>Discover Nearby Tourist Attractions</h3>
-            <p>Explore amazing places waiting to be discovered just around the corner!</p>
-            <Slider attractions={data?.data || []} />
-        </div>
-      )
-  } 
+      fetchAttractions();
+    }, []);
+
+    const normalizedAttractionData = (attractionsData: AttractionsData[]): NormalizedItem[] => {
+      return attractionsData.map(item => ({
+        imageUrl: item.photo?.images?.large?.url || '',
+        title: item.name || '',
+        rating: item.rating || '',
+        num_reviews: item.num_reviews || '',
+        subcategory: item.subcategory ? item.subcategory[0]?.name : ''
+      }));
+    }
+    
+    // if (isPending) {
+    //   return <h3>Is loading...</h3>
+    // }
+
+    // if (isError) {
+    //   return <h3>Error occurred while fetching data.</h3>;
+    // }
+      const normalizeData = normalizedAttractionData(attractionsData);
+
+      if (normalizeData.length === 0) {
+        return <h3>Loading...</h3>; 
+      }
+
+    
+        return (
+            <SectionWithSlider 
+                header="Discover Nearby Tourist Attractions"
+                description="Explore amazing places waiting to be discovered just around the corner!"
+                sliderData={normalizeData}
+                sliderId="slider1"
+            />
+            
+        )
+      
 }
