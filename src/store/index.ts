38d58a08b64ba66f0e0a9da357be6e69e5a0de57
Marcin Wojtczak <marcinwojtczak.pure@gmail.com>
2024-10-06 19:18:24 +0200

    feat: integrate TravelInfoApi into Redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { TravelApi } from '@utils/api/fetch-travelAdvisor-data'; 
+import { TravelInfoApi } from '../utils/api/fetch-tarvelInfo-data'; 
 
 const store = configureStore({
   reducer: {
     [TravelApi.reducerPath]: TravelApi.reducer,
+    [TravelInfoApi.reducerPath]: TravelInfoApi.reducer,
   },
   middleware: (getDefaultMiddleware: any) => 
     getDefaultMiddleware()
       .concat(TravelApi.middleware)
+      .concat(TravelInfoApi.middleware)
 });
 
 export default store
