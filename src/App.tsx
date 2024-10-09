abea9b3741d636ff868ae2596c442eb15d4e37b1
Marcin Wojtczak <marcinwojtczak.pure@gmail.com>
2024-10-09 13:01:25 +0200

    feat:  implement routing with react-router-dom and restructure app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,55 @@
-import HeroSection from '@pages/home/HeroSection';
-import NearbyAtractions from '@pages/home/NearbyAttractions';
-import CountryDetail from './pages/home/CountryDetail';
+import { createBrowserRouter, RouterProvider, Outlet  } from "react-router-dom";
+import HomePage from '@pages/home/HomePage';
+import ThingsToDoPage from "./pages/thingsToDo/ThingsToDoPage";
+import Header from '@components/Header'
+import { fetchGeolocation } from "./features/location/locationSlice";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { RootState } from './store/store';
+import { useSelector } from 'react-redux';
+import ExploreCountryPage from "@pages/exploreCuntry/ExploreCountryPage";
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet /> 
+    </>
+  )
+}
+
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: 'things-to-do/:title',
+        element: <ThingsToDoPage />
+      },
+      {
+        path: 'explore-country/:title',
+        element: <ExploreCountryPage />
+      }
+    ]
+  }
+]);
 
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchGeolocation())
+  }, [dispatch])
   
   return (
-    <div >
-       <HeroSection />
-       <div className=' mx-auto flex flex-col gap-20 my-10 lg:my-20'> 
-          <CountryDetail />
-          <NearbyAtractions />
-       </div>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
