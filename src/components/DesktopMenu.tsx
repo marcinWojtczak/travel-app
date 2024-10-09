ea23ff455725dab58d6c5a4943e5924e7d7e923c
Marcin Wojtczak <marcinwojtczak.pure@gmail.com>
2024-10-09 13:02:36 +0200

    feat: add navigation links to DesktopMenu component

--- src/components/DesktopMenu.tsx
+++ src/components/DesktopMenu.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from 'react-router-dom'
+
 type DesktopMenuProps = {
   border: boolean
 }
@@ -6,9 +8,26 @@ export default function DesktopMenu({ border }: DesktopMenuProps) {
   return (
     <nav className={`hidden lg:inline border ${border ? 'border-dark-green' : 'border-white'} bg-white rounded-full px-8 py-3`} style={{ backgroundColor: 'rgba(255, 255, 255, 0.4)' }}>
         <ul className='flex gap-8 '>
-        <li className='font-medium'>Home</li>
-        <li className='font-medium'>Discover</li>
-        <li className='font-medium'>More</li>
+          <li className='font-medium'>
+            <NavLink 
+              to='/'
+              className={({isActive}) => isActive ? 'underline underline-offset-4 decoration-2 decoration-green' : ''}
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className='font-medium'>
+            <NavLink 
+              to='things-to-do'
+              className={({isActive}) => isActive ? 'underline underline-offset-4 decoration-2 decoration-green' : ''}
+            >
+              Things To Do
+            </NavLink>
+          </li>
+          <li className='font-medium'>
+            More
+          </li>
         </ul>
     </nav>
   )
