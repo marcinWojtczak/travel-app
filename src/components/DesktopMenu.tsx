94ed196b53432f1427805e0d9f6e2eadd48f4aab
Marcin Wojtczak <marcinwojtczak.pure@gmail.com>
2024-10-14 11:58:38 +0200

    style: changed nav style

--- src/components/DesktopMenu.tsx
+++ src/components/DesktopMenu.tsx
@@ -1,12 +1,12 @@
 import { NavLink } from 'react-router-dom'
 
-type DesktopMenuProps = {
-  border: boolean
-}
+export default function DesktopMenu() {
 
-export default function DesktopMenu({ border }: DesktopMenuProps) {
+
   return (
-    <nav className={`hidden lg:inline border ${border ? 'border-dark-green' : 'border-white'} bg-white rounded-full px-8 py-3`} style={{ backgroundColor: 'rgba(255, 255, 255, 0.4)' }}>
+    <nav 
+      className='hidden lg:inline  bg-white rounded-full px-8 py-3' 
+      style={{ backgroundColor: 'rgba(255, 255, 255, 0)' }}>
         <ul className='flex gap-8 '>
           <li className='font-medium'>
             <NavLink 
