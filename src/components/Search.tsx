78ede66b888d1f852a83b0a1bf3daa8c0acf87a4
Marcin Wojtczak <marcinwojtczak.pure@gmail.com>
2024-10-14 12:01:46 +0200

    style: improve  search style

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -8,26 +8,30 @@ type SearchType = {
     border: boolean
 } & InputHTMLAttributes<HTMLInputElement> 
 
-export default function Search({ placeholder, type, customWidth, border, ...props}: SearchType) {
+export default function Search({ placeholder, type, customWidth, border, ...props }: SearchType) {
   const [isFocused, setIsFocused] = useState<boolean>(false)
+  const [hasText, setHasText] = useState<boolean>(false)
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHasText(e.target.value.length > 0)
+  }
 
   return (
-    <div className={`relative flex items-center b h-12 ${customWidth}`}>
+    <div className={`relative flex items-center h-12 ${customWidth}`}>
       <label htmlFor='search' className='sr-only'>Search</label>
       <input
           id='search'
-          className={`p-[10px] h-full border ${border ? 'border-dark-green' : 'border-white' } rounded-full pl-14 outline-none w-full`} 
+          className={`p-[10px] h-full border border-dark-green rounded-full pl-14 outline-none w-full ${hasText ? '' : 'placeholder-visible'}`}
           placeholder={placeholder}
-          style={{ backgroundColor: isFocused ? 'rgba(255, 255, 255, 1.0)' : 'rgba(255, 255, 255, 0.4)'}}
-          type={type}
+          style={{ backgroundColor: isFocused ? 'rgba(255, 255, 255, 0.5)' : 'rgba(255, 255, 255, 0)' }} 
           onFocus={() => setIsFocused(true)} 
           onBlur={() => setIsFocused(false)}
+          onChange={handleInputChange} 
           {...props}
       />
       <div className='absolute bg-brown h-9 w-9 rounded-full flex items-center justify-center pointer-events-none ml-2'>
         <SearchIcon className='h-4 w-4 text-white'/> 
       </div>
-      
     </div>
   )
 }
